695e2d7bcd87dd71ba84fbe2ae4497ad236d4f90
shishir54234 <98634654+shishir54234@users.noreply.github.com>
2022-06-15 20:01:19 +0530

    first commit

---
+++ templates/template.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n) for(i=0;i<n;i++)
+#define pb push_back
+#define mp make_pair
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl /* YOU CAN USE THESE FOR DEBUGGING */
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define all(x) x.begin(), x.end()
+typedef vector<int>		vi;
+typedef map<int,int> mi;
+typedef unordered_map<int,int> u_mi;
+typedef pair<int, int>	pii;
+const int mod = 1'000'000'007;
+void solve()
+{
+
+}
+
+int main()
+{
+cin.tie(0); 
+    cout.tie(0); 
+    ios_base::sync_with_stdio(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif 
+    int n; cin>>n;
+    while(n--)
+        solve();
+}
+
